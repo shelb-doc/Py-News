78cd1baff943437dec71b6df090852686a2829d5
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-04-23 12:48:54 -0400

    connected database to flask to display the seeded data

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from app.routes import home, dashboard
+from app.db import init_db
 
 def create_app(test_config=None):
-      # set up app config
+  # set up app config
   app = Flask(__name__, static_url_path='/')
   app.url_map.strict_slashes = False
   app.config.from_mapping(
@@ -15,5 +16,7 @@ def create_app(test_config=None):
   # register routes
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+
+  init_db(app)
 
   return app
