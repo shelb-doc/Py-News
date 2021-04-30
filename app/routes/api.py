bfa534d8d502abb880e5def8e58e6ba46cce959a
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-04-30 08:35:56 -0400

    added session details

--- app/routes/api.py
+++ app/routes/api.py
@@ -21,10 +21,10 @@ def signup():
     db.add(newUser)
     db.commit()
   except:
-   print(sys.exc_info()[0])
+    print(sys.exc_info()[0])
   # insert failed, so rollback and send error to front end
-  db.rollback()
-  return jsonify(message = 'Signup failed'), 500
+    db.rollback()
+    return jsonify(message = 'Signup failed'), 500
 
   # Session log
   session.clear()
@@ -32,3 +32,29 @@ def signup():
   session['loggedIn'] = True
 
   return jsonify(id = newUser.id)
+
+# logout
+@bp.route('/users/logout', methods=['POST'])
+def logout():
+  # remove session variables
+  session.clear()
+  return '', 204
+
+# login
+@bp.route('/users/login', methods=['POST'])
+def login():
+  data = request.get_json()
+  db = get_db()
+  try:
+    user = db.query(User).filter(User.email == data['email']).one()
+  except:
+    print(sys.exc_info()[0])
+    return jsonify(message = 'Incorrect credentials'), 400
+
+  if user.verify_password(data['password']) == False:
+    return jsonify(message = 'Incorrect credentials'), 400
+
+  session.clear()
+  session['user_id'] = user.id
+  session['loggedIn'] = True
+  return jsonify(id = user.id)
